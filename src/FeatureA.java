8236d58cb7fac610f3f7dc00fb6487a8cc0f5a57
Yaroslava <yaroslava.moskalova@gmail.com>
2025-09-08 12:45:48 +0200

    updated FeatureA.java

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -1,8 +1,8 @@
 public class FeatureA{
 
     public static void main(String[] args) {
-        System.out.println("Hallo, das ist der Code von Florian");
         System.out.println("Hallo, das ist der Code von Nina");
+        System.out.println("Hello, we do not need Florians code anymore");
     }
 
 }
